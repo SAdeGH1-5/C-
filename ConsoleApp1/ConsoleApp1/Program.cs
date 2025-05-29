bdae0b6379416c5bf08db90a9d8aad8524cf04ad
PhantomSecure <s1386job@gmail.com>
2025-05-29 11:03:42 +0430

    0.2

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -1,58 +1,77 @@
-﻿using System;
+﻿// using System;
 
-class Program
-{
-    static void Main()
-    {
-        string[] MyArray = new string[]{}; 
+// class Program
+// {
+//     static void Main()
+//     {
+//         string[] MyArray = new string[]{}; 
 
-        bool run = true;
+//         bool run = true;
         
-        while (run)
-        {
-            Start_app.Start();
-            string inp_user = Console.ReadLine();
-            switch(inp_user)
-            {
-                case "1":
-                    Console.WriteLine(string.Join(", ", MyArray));
-                    break;
-                case "2":
-                    Console.WriteLine("Enter your number for add to array : ");
-                    string arr_inp = Console.ReadLine();
-                    List<string> add_list = MyArray.ToList();
-                    add_list.Add(arr_inp);
-                    MyArray = add_list.ToArray();
-                    break;
-                case "3":
-                    Console.WriteLine("Enter your index as left to right for delete : ");
-                    int del_inp = 1 - Int32.Parse(Console.ReadLine());
-                    List<string> del_list = MyArray.ToList();
-                    del_list.RemoveAt(del_inp);
-                    MyArray = del_list.ToArray();
-                    break;
-                case "4":
-                    Console.WriteLine("The program has stopped.");
-                    run = false;
-                    break;
-            }
-        }
+//         while (run)
+//         {
+//             Start_app.Start();
+//             string inp_user = Console.ReadLine();
+//             switch(inp_user)
+//             {
+//                 case "1":
+//                     Console.WriteLine(string.Join(", ", MyArray));
+//                     break;
+//                 case "2":
+//                     Console.WriteLine("Enter your number for add to array : ");
+//                     string arr_inp = Console.ReadLine();
+//                     List<string> add_list = MyArray.ToList();
+//                     add_list.Add(arr_inp);
+//                     MyArray = add_list.ToArray();
+//                     break;
+//                 case "3":
+//                     Console.WriteLine("Enter your index as left to right for delete : ");
+//                     int del_inp = 1 - Int32.Parse(Console.ReadLine());
+//                     List<string> del_list = MyArray.ToList();
+//                     del_list.RemoveAt(del_inp);
+//                     MyArray = del_list.ToArray();
+//                     break;
+//                 case "4":
+//                     Console.WriteLine("The program has stopped.");
+//                     run = false;
+//                     break;
+//             }
+//         }
 
+//     }
+// }
+
+// class Start_app
+// {
+//     public static void Start(){
+//         Console.WriteLine("""
+//         Welcome to my app !
+//           Menu :
+//             1. Show array
+//             2. Add number
+//             3. Delete number
+//             4. Exit
+        
+//           Select (1_4) :
+//         """);
+//     }
+// }
+
+class Program2
+{
+    static void Main() 
+    {
+        array.arrays();
     }
 }
 
-class Start_app
+class array
 {
-    public static void Start(){
-        Console.WriteLine("""
-        Welcome to my app !
-          Menu :
-            1. Show array
-            2. Add number
-            3. Delete number
-            4. Exit
-        
-          Select (1_4) :
-        """);
+    static int[] arr = {1,2,31,5,4,};
+    public static void arrays(){
+        foreach (int x in arr){
+            Console.WriteLine(x);
+        }
+        Console.WriteLine("[ "+string.Join(", ", arr)+" ]");
     }
 }
